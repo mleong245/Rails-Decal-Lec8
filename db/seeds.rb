3787ddef58553119bae9ed469be7a289cab00490
Sam Lau <samlau95@gmail.com>
2014-10-30 02:45:28 -0700

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create! name: 'Sam', email: 'sam@sam.com', password: 'password'
