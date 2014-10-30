ccb4c45a7c289e2ad2cf064367c0ee5584218ca9
Sam Lau <samlau95@gmail.com>
2014-10-30 01:37:15 -0700

    Migration for user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030080731) do
+ActiveRecord::Schema.define(version: 20141030083615) do
 
   create_table "quits", force: true do |t|
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "quits", ["user_id"], name: "index_quits_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
