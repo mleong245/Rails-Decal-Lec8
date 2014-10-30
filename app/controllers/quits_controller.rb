735198cc48b9de968772f0dfd2f38c2b19a3a1dc
Sam Lau <samlau95@gmail.com>
2014-10-30 03:23:23 -0700

    Update action

--- app/controllers/quits_controller.rb
+++ app/controllers/quits_controller.rb
@@ -2,4 +2,20 @@ class QuitsController < ApplicationController
   def edit
     @quit = Quit.find params[:id]
   end
+
+  def update
+    @quit = Quit.find params[:id]
+    if @quit.update quit_params
+      flash[:success] = 'Updated!'
+      redirect_to @quit.user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def quit_params
+    params.require(:quit).permit(:text)
+  end
 end
