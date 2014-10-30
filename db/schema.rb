f33459fcb4599f371d3daf81044162a11003d071
Sam Lau <samlau95@gmail.com>
2014-10-30 01:25:10 -0700

    Remove email from User so devise works

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20141030075000) do
 
   create_table "users", force: true do |t|
     t.string   "name"
-    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
